c722623a02c89683d29565705dab08c6bdb69899
Luna <luna@luvies.io>
2020-06-14 15:43:43 +0100

    Bumped std version

--- lib/deps/std.ts
+++ lib/deps/std.ts
@@ -1,10 +1,10 @@
-export { decode } from "https://deno.land/std@0.56.0/encoding/utf8.ts";
+export { decode } from "https://deno.land/std@0.57.0/encoding/utf8.ts";
 export {
   blue,
   dim,
   green,
   magenta,
   red,
-} from "https://deno.land/std@0.56.0/fmt/colors.ts";
-export { ensureDir } from "https://deno.land/std@0.56.0/fs/ensure_dir.ts";
-export { globToRegExp } from "https://deno.land/std@0.56.0/path/glob.ts";
+} from "https://deno.land/std@0.57.0/fmt/colors.ts";
+export { ensureDir } from "https://deno.land/std@0.57.0/fs/ensure_dir.ts";
+export { globToRegExp } from "https://deno.land/std@0.57.0/path/glob.ts";
